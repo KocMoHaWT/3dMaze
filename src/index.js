b83b25804bac9717d155ac26ad7f31a0e79da746
Vladislav Dontsov <dontsov.vladislav@gmail.com>
2020-02-20 22:36:22 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,189 @@
+import {
+    Engine
+} from "@babylonjs/core/Engines/engine";
+import {
+    Scene
+} from "@babylonjs/core/scene";
+import {
+    Vector3
+} from "@babylonjs/core/Maths/math";
+import {
+    FreeCamera
+} from "@babylonjs/core/Cameras/freeCamera";
+import {
+    HemisphericLight
+} from "@babylonjs/core/Lights/hemisphericLight";
+import {
+    Mesh
+} from "@babylonjs/core/Meshes/mesh";
+
+import { MeshBuilder } from '@babylonjs/core/Meshes/meshBuilder';
+import {
+    GridMaterial
+} from "@babylonjs/materials/grid";
+
+// Required side effects to populate the Create methods on the mesh class. Without this, the bundle would be smaller but the createXXX methods from mesh would not be accessible.
+import "@babylonjs/core/Meshes/meshBuilder";
+import "@babylonjs/core/Materials/standardMaterial"
+
+// Get the canvas element from the DOM.
+const canvas = document.getElementById("renderCanvas");
+//
+// // Associate a Babylon Engine to it.
+const engine = new Engine(canvas);
+//
+// // Create our first scene.
+// var scene = new Scene(engine);
+//
+// // This creates and positions a free camera (non-mesh)
+// var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+//
+//
+// // This targets the camera to scene origin
+// camera.setTarget(Vector3.Zero());
+//
+// // This attaches the camera to the canvas
+// camera.attachControl(canvas, true);
+//
+// // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+// var light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+//
+// // Default intensity is 1. Let's dim the light a small amount
+// light.intensity = 0.7;
+//
+// // Crea var material = new GridMaterial("grid", scene);te a grid material
+// var material = new GridMaterial("grid", scene);
+//
+// // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
+// var sphere = Mesh.CreateSphere("sphere1", 16, 2, scene);
+//
+// // Move the sphere upward 1/2 its height
+// sphere.position.y = 2;
+//
+// // Affect a material
+// sphere.material = material;
+//
+// // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
+// var ground = Mesh.CreateGround("ground1", 6, 6, 2, scene).material = material;
+//
+// // Affect a material
+// ground.material = material;
+var createScene = function () {
+    // }
+
+    var scene = new Scene(engine);
+    var material = new GridMaterial("grid", scene);
+  //   var camera = new FreeCamera("Camera", Vector3.Zero(), scene);
+	// camera.attachControl(canvas, true);
+    var camera = new FreeCamera("camera1", new Vector3(0, 5, -100), scene);
+    camera.setTarget(Vector3.Zero());
+
+	// var light = new HemisphericLight("hemi", new Vector3(0, 50, 0), scene);
+    var light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+// Array of paths to construct extrusion
+	var myShape = [
+		 	new Vector3(0, 5, 0),
+			new Vector3(1, 1, 0),
+			new Vector3(5, 0, 0),
+			new Vector3(1, -1, 0),
+			new Vector3(0, -5, 0),
+			new Vector3(-1, -1, 0),
+			new Vector3(-5, 0, 0),
+			new Vector3(-1, 1, 0)
+	];
+
+
+	myShape.push(myShape[0]);
+    const item = (x, y, c = []) => ({ x, y, children: c })
+    const testObj = {
+        width: 5,
+        height: 5, 
+        path: item(0, 0, [
+            item(1, 0, [
+                item(1,1, [
+                    item(1,2, [
+                        item(0,2), item(2,2)
+                    ])
+                ])
+            ])
+        ])
+    }
+
+
+    
+    const otherPoint = (obj, parent) => ({x: obj.x, y: obj.y, parentX: parent.x, parentY: parent.y})
+    let processingStack = [testObj.path];
+    let arrayOfSnags = [];
+    let snag = [];
+
+    const createSnag = (startPoint) => {
+        return [startPoint];
+    }
+    
+    // const closeSnag = (snag, arrayOfSnags) => {
+    //     arrayOfSnags.push(snag);
+    //     return arrayOfSnags;
+    // }
+
+    const processElement = (snag, item) => {
+        snag.push([item.x, item.y]);
+        return snag;
+     };
+    const stackElementChildren = (processingStack, item) => {
+        const children = item.children;
+        children.forEach(childItem => processingStack.push({ ...childItem, parent: item }));
+        return processingStack;
+    };
+
+    const createSnagIfNeeded = (snag, item) => {
+        const { parent } = item;
+        return !snag ? createSnag([parent.x, parent.y]) : snag;
+    };
+
+    const closeSnagIfNeeded = (snag, item, arrayOfSnags) => {
+        // console.log('!item.children', !item.children);
+        // console.log('!item.children.length', !item.children.length)
+        if (!item.children || !item.children.length) {
+        arrayOfSnags.push(snag);
+         return [ null, arrayOfSnags ];
+        }
+        return [snag ,arrayOfSnags];
+    };
+
+    while (processingStack.length) {
+        const item = processingStack.pop();
+        snag = createSnagIfNeeded(snag, item);
+        snag = processElement(snag, item);
+        processingStack = stackElementChildren(processingStack, item); 
+        [snag, arrayOfSnags] = closeSnagIfNeeded(snag, item, arrayOfSnags);
+    }
+
+    // ('array of snags: ', arrayOfSnags);
+    const scale = 10;
+    const snagToVectorArray = (snag) => {
+        return snag.map(([x, y]) => new Vector3(x * scale, y * scale, 0))
+    }
+
+    const buildMesh = (vectorArray) => MeshBuilder.ExtrudeShape("star", {shape: myShape, path: vectorArray, sideOrientation: Mesh.DOUBLESIDE, updatable: true}, scene);
+    const arrayOfSnagsVectors = arrayOfSnags
+    .map(snagToVectorArray)
+    .map((vector) => {
+        buildMesh(vector);
+        buildMesh.material = material;
+    });
+
+    var ground = Mesh.CreateGround("ground1", 6, 6, 2, scene);
+
+    ground.material = material;
+
+	return scene;
+};
+
+var scene = createScene();
+console.log(scene);
+// Render every frame
+engine.runRenderLoop(() => {
+    scene.render();
+    
+});
+
